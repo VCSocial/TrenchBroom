0b841232f460f58f960f710913abd299101885f9
Kristian Duske <kristian.duske@gmail.com>
2020-12-23 14:50:59 +0100

    558: Add Group::m_transformation
    This new member will hold a group's transformation, which is used to
    update the other link set members when a group is changed.

--- common/test/src/Model/GroupNodeTest.cpp
+++ common/test/src/Model/GroupNodeTest.cpp
@@ -17,11 +17,20 @@
  along with TrenchBroom. If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include "Model/Brush.h"
+#include "Model/BrushNode.h"
+#include "Model/Entity.h"
 #include "Model/EntityNode.h"
+#include "Model/Group.h"
 #include "Model/GroupNode.h"
 
 #include <vecmath/bbox.h>
 #include <vecmath/bbox_io.h>
+#include <vecmath/mat.h>
+#include <vecmath/mat_ext.h>
+#include <vecmath/mat_io.h>
+
+#include <kdl/result.h>
 
 #include <memory>
 #include <vector>
@@ -110,6 +119,53 @@ namespace TrenchBroom {
             groupNodeClone->unlink();
             CHECK_FALSE(groupNodeClone->linked());
             CHECK_THAT(groupNodeClone->linkedGroups(), Catch::UnorderedEquals(std::vector<GroupNode*>{&groupNode}));
+        }
+
+        static void transform(Node& node, const vm::mat4x4& transformation, const vm::bbox3& worldBounds) {
+            node.accept(kdl::overload(
+                [](const WorldNode*) {},
+                [](const LayerNode*) {},
+                [&](auto&& thisLambda, GroupNode* groupNode) {
+                    auto group = groupNode->group();
+                    group.transform(transformation);
+                    groupNode->setGroup(std::move(group));
+
+                    groupNode->visitChildren(thisLambda);
+                },
+                [&](auto&& thisLambda, EntityNode* entityNode) {
+                    auto entity = entityNode->entity();
+                    entity.transform(transformation);
+                    entityNode->setEntity(std::move(entity));
+
+                    entityNode->visitChildren(thisLambda);
+                },
+                [&](BrushNode* brushNode) {
+                    auto brush = brushNode->brush();
+                    REQUIRE(brush.transform(worldBounds, transformation, false).is_success());
+                    brushNode->setBrush(std::move(brush));
+                }
+            ));
+        }
+
+        TEST_CASE("GroupNodeTest.transform", "[GroupNodeTest]") {
+            const auto worldBounds = vm::bbox3(8192.0);
+
+            auto groupNode = GroupNode(Group("name"));
+            REQUIRE(groupNode.group().transformation() == vm::mat4x4());
+
+            auto* entityNode = new EntityNode();
+            groupNode.addChild(entityNode);
+            
+            transform(groupNode, vm::translation_matrix(vm::vec3(32.0, 0.0, 0.0)), worldBounds);
+            CHECK(groupNode.group().transformation() == vm::translation_matrix(vm::vec3(32.0, 0.0, 0.0)));
+
+            transform(groupNode, vm::rotation_matrix(0.0, 0.0, vm::to_radians(90.0)), worldBounds);
+            CHECK(groupNode.group().transformation() == vm::rotation_matrix(0.0, 0.0, vm::to_radians(90.0)) * vm::translation_matrix(vm::vec3(32.0, 0.0, 0.0)));
+
+            auto testEntityNode = EntityNode();
+            transform(testEntityNode, groupNode.group().transformation(), worldBounds);
+
+            CHECK(testEntityNode.entity() == entityNode->entity());
         }
     }
 }

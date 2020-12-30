99695403671e376cdc6c98191d62277046197b04
Kristian Duske <kristian.duske@gmail.com>
2020-12-30 18:43:58 +0100

    558: Don't issue SwapNodeCommands when nothing needs to be done
    This change also uncovered an issue in a test, which I fixed as well.
    
    This is just a minor improvement for corner cases in which no work is
    actually done.

--- common/src/View/MapDocument.cpp
+++ common/src/View/MapDocument.cpp
@@ -172,6 +172,10 @@ namespace TrenchBroom {
          */
         template <typename N, typename L>
         static bool applyAndSwap(MapDocument& document, const std::string& commandName, const std::vector<N*>& nodes, L lambda) {
+            if (nodes.empty()) {
+                return true;
+            }
+
             if (auto newNodes = applyToNodeContents(nodes, std::move(lambda))) {
                 document.swapNodeContents(commandName, std::move(*newNodes));
                 return true;
@@ -196,6 +200,10 @@ namespace TrenchBroom {
          */
         template <typename L>
         static bool applyAndSwap(MapDocument& document, const std::string& commandName, const std::vector<Model::BrushFaceHandle>& faces, L lambda) {
+            if (faces.empty()) {
+                return true;
+            }
+
             auto brushes = std::unordered_map<Model::BrushNode*, Model::Brush>{};
 
             bool success = true;
